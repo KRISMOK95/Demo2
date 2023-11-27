a94b9c781d4aa45f198147d99fba7d05117ecbc6
KRISMOK95 <kaxm462@gmail.com>
2023-11-27 20:50:03 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -5,7 +5,14 @@
  **/
 const nextConfig = {
   /**
-   * Set base path. This should match the name of your GitHub repository.
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is usually the slug of your repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
